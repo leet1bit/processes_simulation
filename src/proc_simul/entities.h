daeafd439095205622a2f0dd519133220ed73954
anas <anasbelkhadir13@gmail.com>
2025-12-14 15:29:00 +0100

    separating entities to be more humain friendly

--- src/proc_simul/entities.h
+++ src/proc_simul/entities.h
@@ -15,19 +15,15 @@ enum {
     READY, BLOCKED, EXECUTION, TERMINATED
 } E_etat;
 
-typedef struct {
-    char ressource_name[10];
-    bool disponibilite;
+enum {
+    // ressources
 } RESSOURCE;
 
 typedef struct {
-    struct tm temps_arrive; // located in time.h
-    struct tm temps_creation;
-    struct tm temps_fin;
-    
-    int temps_attente; // gap entre temps arrive & temps exec
-    int tournround; // temps terminer - temps arrive
-} PROCESS_STATISTICS;
+    char* ressource_name[10]; // nom de ressource
+    RESSOURCE* ressource; // type ooubien l enum
+    bool disponibilite; // dispo ou non 1 / 0
+} RESSOURCES_ELEMENT;
 
 enum {
     // should define types
@@ -42,10 +38,18 @@ typedef struct {
     int process_it; // the id of the process owner
 
     float time_remaining; // in nano seconds
-    INSTRUCTION_TYPE type; // type of instruction which is ressource needed
-    INSTRUCTION_STATE state; // state of instruction  KHLINAHA HNA
+    INSTRUCTION_TYPE* type; // type of instruction which is ressource needed
+    INSTRUCTION_STATE* state; // state of instruction 
+} INSTRUCTION;
+
+typedef struct {
+    struct tm temps_arrive; // located in time.h
+    struct tm temps_creation;
+    struct tm temps_fin;
     
-} INSTRUCTION_DETAILS;
+    int temps_attente; // gap entre temps arrive & temps exec
+    int tournround; // temps terminer - temps arrive
+} PROCESS_STATISTICS;
 
 typedef struct {
     int pid;
@@ -55,9 +59,10 @@ typedef struct {
     E_etat etat; // one of the values in the enum e_etat
     int prioritie; // priorities from 1 to 5 ; 1-critical
 
-    char* instructions;
+    INSTRUCTION* instructions;
     long programme_compteur; // max 9,223,372,036,854,775,807 instruction
     int memoire_necessaire; // en MB
+    INSTRUCTION* current_instruction; // l'instruction en train de s executer
 
     int burst_time; // total temps necessaire en ms pour l'exec . burst = compte_temps + temps_restant
     int cpu_time_used; // temps cpu consomme en ms init 0
@@ -94,10 +99,7 @@ typedef struct { // ordred chaine
     int size; // nombre du nodes
 } READY_QUEUE;
 
-typedef struct {
-    char ressource_name[10];
-    bool disponibilite;
-} RESSOURCES_ELEMENT;
+
 
 typedef struct {
     int pid; // l id processus
