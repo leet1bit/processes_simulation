5e2e3bae406a3e09f0d77a4ef07d6ca75c0b3e34
anas <anasbelkhadir13@gmail.com>
2025-12-16 01:19:44 +0100

    cooking in process_manager helper

--- lib/operations/helpers/process_manager.h
+++ lib/operations/helpers/process_manager.h
@@ -12,42 +12,6 @@ typedef struct {
     int size;
 } buffer_return;
 
-
-typedef struct {
-    int pid;
-    char process_name[20];
-    char user_id[20];
-    int ppid;
-    E_etat etat; // one of the values in the enum e_etat
-    int prioritie; // priorities from 1 to 5 ; 1-critical
-
-    INSTRUCTION* instructions;
-    long programme_compteur; // max 9,223,372,036,854,775,807 instruction
-    int memoire_necessaire; // en MB
-    INSTRUCTION* current_instruction; // l'instruction en train de s executer
-
-    int burst_time; // total temps necessaire en ms pour l'exec . burst = compte_temps + temps_restant
-    float cpu_time_used; // temps cpu consomme en ms init 0
-    int remaining_time; // temps restant : = burst - cpu_time_used
-    int cpu_usage; // initialized as 0
-
-    PROCESS_STATISTICS* statistics; 
-
-    int* pid_childrens;
-    PCB* pid_sibling_next; // pointeur vers next sib
-    PCB* pid_sibling_previous; // pointeur vers previous sib
-} PCB;
-
-typedef struct {
-    uint32_t* instruct_id; // the id of instruction because process can stop at it if need ressources, pointer because it can be too long
-    PCB* process; // the id of the process owner
-
-    float time_remaining; // in nano seconds
-    RESSOURCE_ELEMENT* type; // type of instruction which is ressource needed
-    INSTRUCTION_STATE* state; // state of instruction 
-    INSTRUCTION* next;
-} INSTRUCTION;
-
 buffer_return* extract_from_buffer(FILE* csv_buffer) {
     
     int count = 0;
